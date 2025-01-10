c269b462b865635e629405cc75ee5170912f5ac9
janggmii <janggmii17@gmail.com>
2025-01-11 04:04:08 +0700

    login regist dao

---
+++ app/src/main/java/com/example/projectanmp/model/LoginDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.projectanmp.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Login::class], version = 1)
+abstract class LoginDatabase : RoomDatabase() {
+    abstract fun todoDao(): LoginDao
+
+    companion object {
+        @Volatile
+        private var instance: LoginDatabase? = null
+        private val LOCK = Any()
+
+
+        fun getInstance(context: Context): LoginDatabase {
+            return instance ?: synchronized(LOCK) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+
+        private fun buildDatabase(context: Context): LoginDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                LoginDatabase::class.java,
+                "newLoginDB"
+            ).build()
+        }
+    }
+}
--- app/src/main/java/com/example/projectanmp/model/Model.kt
+++ app/src/main/java/com/example/projectanmp/model/Model.kt
@@ -1,5 +1,9 @@
 package com.example.projectanmp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class EsportGamesResponse(
     val esport_games: List<EsportGame>
 )
@@ -34,3 +38,15 @@ data class Team(
     val name: String,
     val members: List<String>
 )
+
+@Entity
+data class Login(
+    @ColumnInfo(name="username")
+    var usrname:String,
+    @ColumnInfo(name="password")
+    var passwrd:String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid:Int =0
+}
+
